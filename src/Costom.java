ff1424a427bfc7e053f5c100486cdfe94057b601
laziz746 <38212585+laziz746@users.noreply.github.com>
2019-04-04 19:05:48 +0300

    Идея намного проще

--- src/Costom.java
+++ src/Costom.java
@@ -1,6 +1,7 @@
 public class Costom extends Exception {
 
 
+
     public Costom(String message) {
         super(message);
     }
--- src/MyArraySizeException.java
+++ src/MyArraySizeException.java
@@ -3,5 +3,6 @@ public class MyArraySizeException extends Costom {
     public MyArraySizeException() {
         super("Массив больше чем 4х4 ");
 
+
     }
 }
